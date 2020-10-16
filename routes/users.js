03565e2898c3d92eb8ebb3e45de47d980a6dd3a7
hanwang <wangh109@coventry.ac.uk>
2020-10-16 21:08:11 +0800

    version 1.0.3 finish with user route and user model
    next step will implemented  authentication on user

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,6 @@
 const Router = require('koa-router');
 const bodyParser =require('koa-bodyparser');
-
+const Users = require('../models/users')
 // The koa-router provides a router.prefix method, which is a global configuration for a router and is automatically added to all paths of the router.
 const router = Router({prefix:'/api/v1/users'});
 
@@ -11,31 +11,65 @@ router.post('/',bodyParser(),createUser);
 
 //In koa, we often need to map all the routes that match a certain schema to the same method. For example, we have a user's routing widget, and we need to use this method to render the routes for all users with different IDs. So, we can use "dynamic path parameters" in koa's route paths
 router.get('/:id([0-9]{1,})',getById);
-router.put('/:id([0-9]{1,})',updateUser);
+router.put('/:id([0-9]{1,})',bodyParser(),updateUser);
 router.del('/:id([0-9]{1,})',deleteUser);
 
 
 
-function getAllUsers(ctx){
-    ctx.body = 'This is a list of all users';
+async function getAllUsers(ctx){
+    const result = await Users.getAll();
+    if (result.length){
+        ctx.body =result;
+    }
 }
 
-function createUser(ctx){
-    ctx.body= 'You can create a user here!';
-}
+
 //The koa-router supports custom routing with a colon in url, which is mounted on the context and can be easily retrieved from context.params.paramName.
-function getById(ctx){
+
+async function getById(ctx){
     let id= ctx.params.id;
-    ctx.body= 'This is the specific User you looking for'+ id;
+    const result = await Users.getById(id);
+    if(result.length){
+        const user = result[0];
+    }
+    ctx.body = result[0];
 
 }
-function updateUser(ctx){
-    ctx.body= 'Update information for you user';
 
-}
-function deleteUser(ctx){
-    ctx.body= 'You can delete a user here ;)';
 
+async function createUser(ctx) {
+    const body = ctx.request.body;
+    const result = await Users.add(body);
+    if (result.affectedRows) {
+      const id = result.insertId;
+      ctx.status = 201;
+      ctx.body = {ID: id, created: true, link: `${ctx.request.path}/${id}`};
+    }
+  }
+
+
+  async function updateUser(ctx) {
+    const id = ctx.params.id;
+    let result = await Users.getById(id);  // check it exists
+    if (result.length) {
+      let user = result[0];
+      // exclude fields that should not be updated
+      const {ID, dateRegistered, ...body} = ctx.request.body;
+      Object.assign(user, body); // overwrite updatable fields with body data
+      result = await Users.update(user);
+      if (result.affectedRows) {
+        ctx.body = {ID: id, updated: true, link: ctx.request.path};
+      }
+    }
+  }
+
+async function deleteUser(ctx) {
+  const id = ctx.params.id;
+  const result = await Users.delById(id);
+  if (result.affectedRows) {
+    ctx.body = {ID: id, deleted: true}
+  }
 }
+
 
 module.exports = router;
