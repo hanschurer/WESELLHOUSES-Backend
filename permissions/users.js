caf6af6e93e2162f4c15f9c3d2e334dd769ce68e
wangh109 <wangh109@coventry.ac.uk>
2020-12-05 15:00:04 +0800

    'm'

--- permissions/users.js
+++ permissions/users.js
@@ -1,23 +1,64 @@
-
-const AccessControl = require('role-acl');
-const ac = new AccessControl();
+const AccessControl = require('role-acl')
+const ac = new AccessControl()
 
 // controls for CRUD operations on user records
-ac.grant('user').condition({Fn:'EQUALS', args: {'requester':'$.owner'}}).execute('read')
-  .on('user', ['*', '!password', '!passwordSalt']);
-ac.grant('user').condition({Fn:'EQUALS', args: {'requester':'$.owner'}}).execute('update')
-  .on('user', ['firstName', 'lastName', 'about', 'password', 'email', 'avatarURL']);
-  
-ac.grant('admin').execute('read').on('user');
-ac.grant('admin').execute('read').on('users');
-ac.grant('admin').execute('update').on('user');
-ac.grant('admin').condition({Fn:'NOT_EQUALS', args: {'requester':'$.owner'}}).execute('delete').on('user');
+ac.grant('user')
+  .condition({ Fn: 'EQUALS', args: { requester: '$.owner' } })
+  .execute('read')
+  .on('user', ['*', '!password', '!passwordSalt'])
+ac.grant('user')
+  .condition({ Fn: 'EQUALS', args: { requester: '$.owner' } })
+  .execute('update')
+  .on('user', [
+    'firstName',
+    'lastName',
+    'about',
+    'password',
+    'email',
+    'avatarURL'
+  ])
 
+ac.grant('admin')
+  .execute('read')
+  .on('user')
+ac.grant('admin')
+  .execute('read')
+  .on('users')
+ac.grant('admin')
+  .execute('update')
+  .on('user')
+ac.grant('admin')
+  .condition({ Fn: 'NOT_EQUALS', args: { requester: '$.owner' } })
+  .execute('delete')
+  .on('user')
 
-exports.readAll = (requester) => ac.can(requester.role).execute('read').sync().on('users');
+exports.readAll = requester =>
+  ac
+    .can(requester.role)
+    .execute('read')
+    .sync()
+    .on('users')
 
-exports.read = (requester, data) =>  ac.can(requester.role).context({requester:requester.ID, owner:data.ID}).execute('read').sync().on('user');
+exports.read = (requester, data) =>
+  ac
+    .can(requester.role)
+    .context({ requester: requester.ID, owner: data.ID })
+    .execute('read')
+    .sync()
+    .on('user')
 
-exports.update = (requester, data) => ac.can(requester.role).context({requester:requester.ID, owner:data.ID}).execute('update').sync().on('user');
+exports.update = (requester, data) =>
+  ac
+    .can(requester.role)
+    .context({ requester: requester.ID, owner: data.ID })
+    .execute('update')
+    .sync()
+    .on('user')
 
-exports.delete = (requester, data) => ac.can(requester.role).context({requester:requester.ID, owner:data.ID}).execute('delete').sync().on('user');
+exports.delete = (requester, data) =>
+  ac
+    .can(requester.role)
+    .context({ requester: requester.ID, owner: data.ID })
+    .execute('delete')
+    .sync()
+    .on('user')
